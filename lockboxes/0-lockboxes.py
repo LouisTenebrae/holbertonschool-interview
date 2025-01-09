bdb1ae649231f369967dd49f0f19c393893a3259
Louis Martinez Echevarria <louismartinezechevarria@gmail.com>
2025-01-09 10:40:04 -0400

    What about now?

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Lockboxes """
 
-
 def canUnlockAll(boxes):
     """ Method that determines if all boxes can be opened """
+
 
     for key in range(1, len(boxes)):
         flag = False
